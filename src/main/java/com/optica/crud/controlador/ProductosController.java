4123c4490e65b0cf49c2797aa96ddd93becafa01
Jefry <jefry.vminovacion@gmail.com>
2024-10-26 14:31:46 -0500

    Funcion guardar producto, vista crear y logger para la clase ProductoController

--- src/main/java/com/optica/crud/controlador/ProductosController.java
+++ src/main/java/com/optica/crud/controlador/ProductosController.java
@@ -1,15 +1,45 @@
 package com.optica.crud.controlador;
 
+
+import org.slf4j.*;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.optica.crud.modelo.Productos;
+import com.optica.crud.modelo.Usuario;
+import com.optica.crud.servicio.ProductosServicio;
 
 @Controller
 @RequestMapping("/productos")
 public class ProductosController {
-	
+	//creamos objeto tipo logger
+	private final Logger LOGGER = LoggerFactory.getLogger(ProductosController.class);
+	//implementacion en BD
+	@Autowired
+	private ProductosServicio productosservicio;
+	//mapeo 
 	@GetMapping("")
 	public String vista() {
 		return "productos/vista";
 	}
+	
+	@GetMapping("/crear")
+	public String crear() {
+		return "productos/crear";
+	}
+	
+	//metodo para mapear la info desde guardar
+	@PostMapping("/save")
+	public String save(Productos productos) {
+		LOGGER.info("Este objeto es el producto{}", productos);
+		Usuario u=new Usuario(1, "", "", "", "", "", "", "", null);
+		productos.setUsuario(u);
+		
+		productosservicio.save(productos);
+		return "redirect:/productos";
+	}
+	
 }
